bb0aa16c1d9ff6a7fa211f02e35402133781345e
EeeasyCode <eeeasycode@gmail.com>
2023-01-11 14:43:42 +0900

    [FEAT] first init

---
+++ src/test/java/com/easycode/restfuljava/RestfulJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easycode.restfuljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
